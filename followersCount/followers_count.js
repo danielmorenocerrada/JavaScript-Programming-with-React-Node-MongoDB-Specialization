870d42bb7babde7749d001a73180c71b866a495b
danielmorenocerrada <daniel.morenocerrada@gmail.com>
2025-10-24 02:24:00 -0400

    follower counting example

---
+++ followersCount/followers_count.js
@@ -0,0 +1,20 @@
+let count = 0 // Initialize count to 0
+
+function increaseCount() {
+    count++; // Increment the count by 1
+    displayCount();
+    checkCountValue();
+}
+
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML=count; // Display the count in html
+}
+
+function checkCountValue(){
+    if (count == 10) {
+        alert("Your instagram post gained 10 followers! Congratulations");
+
+    }else if (count == 20) {
+        alert("Your instagram post gained 20 followers! Keep it up!");
+    }
+}
